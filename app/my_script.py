dc7d118c18941f57eb5a2fe6138b7873170c978b
Wenhui Fang <46543296+windy030@users.noreply.github.com>
2019-04-08 17:27:23 -0400

    first commit

---
+++ app/my_script.py
@@ -0,0 +1,4 @@
+# testing-123/my_script.py
+
+def enlarge(i):
+    return i * 3
---
+++ test/my_test.py
@@ -0,0 +1,7 @@
+# testing-123/my_test.py
+
+from app.my_script import enlarge # load the `enlarge()` function to avoid NameError: name 'enlarge' is not defined
+
+def test_enlarge(): # note the function name is prefixed with "test_"
+    result = enlarge(3) # directly invoke the function we want to test
+    assert result == 9 # describe expectations for desired behavior
